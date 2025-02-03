d184d5fd0de28d10bfbbc12d0987aa159fd95bb2
imdepak <deepak.v@outlook.com>
2025-02-03 16:08:27 +0530

    Test Checking

--- obj/Release/net8.0/MyDotnetProject.AssemblyInfo.cs
+++ obj/Release/net8.0/MyDotnetProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyDotnetProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5270892eefd89d8e4922e91762c3fc56b2720a80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+242546646ab06a282de932d83ce217861f6715ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyDotnetProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyDotnetProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
