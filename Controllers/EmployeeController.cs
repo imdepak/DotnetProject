a6cce94771ca59dbb16d6086d673f08dfc296f97
imdepak <deepak.v@outlook.com>
2025-02-03 12:47:58 +0530

    Checking CI/CD

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -25,7 +25,7 @@ public List<Employee>  GetEmployees()
 [HttpGet("getEmployees")]
 public String getEmployees()
 {
-    var employees = "Checking My CI/CD Pipeline Testing Now1";
+    var employees = "Checking My CI/CD Pipeline Testing Now 2";
     return employees;
 }
 
--- obj/Release/net8.0/MyDotnetProject.AssemblyInfo.cs
+++ obj/Release/net8.0/MyDotnetProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyDotnetProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+604b9ae288a6a9c50b5aee6a06b7dadfd737a49d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af3016a15a33fdcae9f6087942dc5c85a9401e17")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyDotnetProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyDotnetProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
