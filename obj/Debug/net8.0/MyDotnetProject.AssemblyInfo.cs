40750013f62263b7caecab856ded5492a6adead8
imdepak <deepak.v@outlook.com>
2025-06-24 15:17:48 +0530

    Dev Test

--- obj/Debug/net8.0/MyDotnetProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyDotnetProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyDotnetProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7c03dc54fa629e87dfa10ea301fb1c41377163f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d738414b52b6deb48a8063a539a3895e658b3b25")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyDotnetProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyDotnetProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- obj/Release/net8.0/MyDotnetProject.AssemblyInfo.cs
+++ obj/Release/net8.0/MyDotnetProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyDotnetProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a986e1586ab937c2c49e459054273484d3b98dc4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d738414b52b6deb48a8063a539a3895e658b3b25")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyDotnetProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyDotnetProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
