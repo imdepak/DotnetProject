d738414b52b6deb48a8063a539a3895e658b3b25
imdepak <deepak.v@outlook.com>
2025-06-24 15:11:23 +0530

    Dev Test

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -25,7 +25,7 @@ public List<Employee>  GetEmployees()
 [HttpGet("getEmployees")]
 public String getEmployees()
 {
-    var employees = "Checking My Pipeline Testing Now For Deepak Deepak";
+    var employees = "Checking My Pipeline Testing Now For Deepak";
     return employees;
 }
 
